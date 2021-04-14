b9531c2ed2e4168cb9423de4b0d98de7d3ce987a
mauricio.mage <mauricio.mage@kaplan.edu>
2021-04-14 10:30:59 +0200

    Added ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 class App extends Component {
-  
+  expenses = [
+    { id: 'e1', title: 'Car Insurance', amount: 256.98, date: new Date(2021, 3, 26) },
+    { id: 'e2', title: 'Car Insurance', amount: 256.98, date: new Date(2021, 3, 25) },
+    { id: 'e3', title: 'Car Insurance', amount: 256.98, date: new Date(2021, 3, 25) },
+    { id: 'e4', title: 'Car Insurance', amount: 256.98, date: new Date(2021, 3, 25) }
+  ]
+
   render() {
     return (
-      <div className="App">
-        <h1>I'm a React App</h1>
-        <p>This is really working!</p>
-        <ExpenseItem></ExpenseItem>
+      <div>
+        <h2>This is really working!</h2>
+        <Expenses items={this.expenses} />
       </div>
     );
   }
